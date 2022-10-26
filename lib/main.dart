1d5a6020a273fdc1de93f0e998b16e2b405d454a
cultyvate <mrao1089@gmail.com>
2022-10-26 12:40:25 +0530

    26-10-2022

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,140 @@
+// @dart=2.9
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '/dashboard.dart';
 
-void main() {
-  runApp(const MyApp());
+void main()  {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: login(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class login extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<StatefulWidget> createState() {
+    return _login();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _login extends State<login> {
+  var width,hieght;
+  bool error = false;
+  TextEditingController username = new TextEditingController();
+  TextEditingController password = new TextEditingController();
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    width = MediaQuery.of(context).size.width;
+    hieght = MediaQuery.of(context).size.height;
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: SingleChildScrollView(
+              child: Column(children: <Widget>[
+                SizedBox(
+                  height: 50,
+                ),
+                Image.asset(
+                  'assets/images/cultyvate.png',
+                  height: 50,
+                ),
+                SizedBox(
+                    height: hieght/12
+                ),
+                Card(
+                  // color: Color(0xFFB9F6CA),
+                  color: Colors.white,
+                  elevation: 20,
+                  child: SizedBox(
+                    width: width,
+                    height: hieght/2,
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(
+                          height: 40,
+                        ),
+                        Container(
+                          padding: EdgeInsets.fromLTRB(20, 10, 20, 5),
+                          child: TextFormField(
+                            controller: username,
+                            decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.person),
+                              labelText: 'Username',
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(30)),
+                              counter: Offstage(),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.fromLTRB(20, 10, 20, 5),
+                          child: TextFormField(
+                            controller: password,
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.key),
+                              labelText: 'Password',
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(30)),
+                              counter: Offstage(),
+                            ),
+                          ),
+                        ),
+                        error
+                            ? Text(
+                          'Invalid Username and Password',
+                          style: TextStyle(color: Colors.red, fontSize: 15),
+                        )
+                            : Text(''),
+                        MaterialButton(
+                          height: hieght/12,
+                          minWidth: 300,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: new BorderRadius.circular(20)
+                          ),
+                          onPressed: () {
+                            if (username.text == "user" &&
+                                password.text == "cultYvate") {
+                              setState(() {
+                                error = false;
+                              });
+                              Get.to(Dashboard());
+                              // Navigator.pushReplacement(
+                              //     context,
+                              //     MaterialPageRoute(builder: (BuildContext context) => Dashboard()));
+                            } else {
+                              setState(() {
+                                error = true;
+                              });
+                            }
+                          },
+                          child: Text(
+                            'Login',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          color: Colors.green,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.center,
+                //   children: [
+                //     IconButton(onPressed: null, icon: Icon(MdiIcons.instagram,size: 35,color: Colors.red,)),
+                //     IconButton(onPressed: null, icon: Icon(MdiIcons.facebook,size: 35,color: Colors.blue,)),
+                //     IconButton(onPressed: null, icon: Icon(MdiIcons.linkedin,size: 35,color: Colors.blueAccent,)),
+                //     IconButton(onPressed: null, icon: Icon(MdiIcons.youtube,size: 35,color: Colors.red,)),
+                //   ],
+                // )
+              ])),
+        ));
   }
 }
